43c31f2c89fd667c2ac7598a3a3655e6fcfa4441
moka236 <medosd236@gmail.com>
2023-10-24 05:38:53 -0700

    more_singly

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: head of a list.
+ *
+ * Return: no return.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+	listint_t *cur;
+
+	if (head != NULL)
+	{
+		cur = *head;
+		while ((tmp = cur) != NULL)
+		{
+			cur = cur->next;
+			free(tmp);
+		}
+		*head = NULL;
+	}
+}
