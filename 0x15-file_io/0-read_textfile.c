7eb303aece1aa394439f0562c7c943531163b5dd
moka236 <medosd236@gmail.com>
2023-10-30 07:59:03 -0700

    file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -21,7 +21,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fu == -1)
 		return (0);
 
-	buf = malloc(sizeof(char) * (letters));
+	bf = malloc(sizeof(char) * (letters));
 	if (!bf)
 		return (0);
 
